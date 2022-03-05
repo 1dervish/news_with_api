7f45444bbbaca029b36ee491670623e1efff09bf
Dərviş <donmss21@gmail.com>
2022-03-05 14:35:58 +0400

    first commit

---
+++ lib/api/api_remote.dart
@@ -0,0 +1,15 @@
+import 'package:news_with_api/api/model/posts.dart';
+
+import 'package:http/http.dart' as http;
+
+class RemoteApiService {
+  Future<List<Posts>?> getPosts() async {
+    var client = http.Client();
+    var uri = Uri.parse("https://jsonplaceholder.typicode.com/posts");
+    var response = await client.get(uri);
+    if (response.statusCode == 200) {
+      var json = response.body;
+      return postsFromJson(json);
+    }
+  }
+}
